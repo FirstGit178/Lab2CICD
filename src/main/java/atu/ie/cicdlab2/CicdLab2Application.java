0488c3ea663a932945c3abd9883316e63812db1c
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-10-01 14:35:43 +0100

    Initial Commit with added dependency for springDoc

---
+++ src/main/java/atu/ie/cicdlab2/CicdLab2Application.java
@@ -0,0 +1,13 @@
+package atu.ie.cicdlab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/cicdlab2/CicdLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.cicdlab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
